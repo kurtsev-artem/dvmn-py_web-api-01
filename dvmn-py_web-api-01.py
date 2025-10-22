ec86922af39899ac8e2e1422384ca31874470b0c
kurtsev-artem <66749796+kurtsev-artem@users.noreply.github.com>
2025-10-22 22:43:35 +0300

    Add files via upload

--- dvmn-py_web-api-01.py
+++ dvmn-py_web-api-01.py
@@ -3,22 +3,17 @@ import requests
    
 
 def main():
-    url_template = 'https://wttr.in/{}?nmMTqu&lang=ru'
-    url = url_template.format('Лондон')
-    response = requests.get(url)
-    response.raise_for_status()
-    print(response.text)
+    
+    locations = ['Лондон','svo','Череповец']
+    payload = {'nmMTqu':'', 'lang': 'ru'}
+    
+    url_template = 'https://wttr.in/{}'
 
-    url = url_template.format('svo')
-    response = requests.get(url)
-    response.raise_for_status()
-    print(response.text)
+    for location in locations:
+        url = url_template.format(location)
+        response = requests.get(url,params=payload)
+        response.raise_for_status()
+        print(response.text)
 
-    url = url_template.format('Череповец')
-    response = requests.get(url)
-    response.raise_for_status()
-    print(response.text)    
-
- 
 if __name__ == '__main__':
     main()    
